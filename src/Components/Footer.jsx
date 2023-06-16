0502470627cbd980f1d78b3f10c862489ef5a1eb
swathireddappa <swathireddy1133@gmail.com>
2023-06-16 11:15:21 +0200

    add nav n footer

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='footer'>
+        
+        <footer className="footer footer-center p-4 bg-accent text-netrual-content">
+  <div>
+    <p>Copyright © 2023 - All right reserved by ACME Industries Ltd</p>
+  </div>
+</footer>
+    </div>
+  )
+}
+
+export default Footer
