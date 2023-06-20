c274d5b5e075eb1cd6e94f732bef0f1b7c53ee61
swathireddappa <swathireddy1133@gmail.com>
2023-06-20 22:53:44 +0200

    insert image n the desc in gdanskp

--- src/Components/Gdanskpage.jsx
+++ src/Components/Gdanskpage.jsx
@@ -1,12 +1,46 @@
-import React from 'react'
+
+import React  from 'react'
+import { useNavigate } from 'react-router-dom'
 
 function Gdanskpage() {
+
+    const navigate = useNavigate()
+  function click(){
+    navigate('/page2')
+
+  }
   return (
-    <div>Gdanskpage
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ab maxime accusamus, nesciunt ea facilis quae repellendus impedit aliquid consequatur expedita iusto ratione nostrum. Voluptatibus deserunt quam quo doloremque libero sunt.
-            <button>Home</button>
-        </p>
+    <div>
+
+        <h2>Gdansk</h2>
+      <div className="h-96 carousel carousel-vertical rounded-box">
+
+       <div className="carousel-item h-96">
+        <img src="https://ik.imagekit.io/8sp02fxgg/gdansk.jpg?updatedAt=1687177426964" />
+        </div> 
+
+         <div className="carousel-item h-96">
+          <img src="https://ik.imagekit.io/8sp02fxgg/tricity2.jpg?updatedAt=1686831886233" />
+        </div> 
+
+         <div className="carousel-item h-96">
+          <img src="https://ik.imagekit.io/8sp02fxgg/tricity3.jpg?updatedAt=1686831886059" />
+          </div> 
+
+        <div className="carousel-item h-96">
+          <img src="https://ik.imagekit.io/8sp02fxgg/tricity5.jpg?updatedAt=1686831886339" />
+         </div> 
+
+      </div>
+            <div className='gdnskpg'>
+              <p className='pgdesc'>With a population of 470,621, Gdańsk is the
+                 capital and largest city of the Pomeranian Voivodeship. 
+                 It is Poland's principal seaport and the country's fourth-largest metropolitan area.
+                </p>
+                      <button onClick={click} className='gdbtn'>Back</button>
+            </div>
     </div>
+  
   )
 }
 
