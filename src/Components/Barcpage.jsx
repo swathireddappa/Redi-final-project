0def0a60af0e8341004e2bfb524e83a9e528d76c
swathireddappa <swathireddy1133@gmail.com>
2023-06-20 21:33:09 +0200

    insert imsge btn n desc of barcelona

--- src/Components/Barcpage.jsx
+++ src/Components/Barcpage.jsx
@@ -2,13 +2,25 @@ import React from 'react'
 
 function Barcpage() {
   return (
-    <div>Barcpage 
+    <div>
+         <h2 className='barche'>Barcelona</h2>
+        <div className='barce'>
+          <img className='barc1' src="https://ik.imagekit.io/8sp02fxgg/barcelona1.webp?updatedAt=1686861494893" alt="" />
+          <img className='barc2' src="https://ik.imagekit.io/8sp02fxgg/bercelona2.avif?updatedAt=1686861494920" alt="" />
+          <img className='barc3' src="https://ik.imagekit.io/8sp02fxgg/barcelona6.jpg?updatedAt=1686861494561" alt="" />
+        </div>
 
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt minima
-             et repellendus amet 
-            impedit voluptas deserunt corporis maiores ipsa quasi ex,
-             hic magni, voluptatum tempore perspiciatis ea ullam. Pariatur, mollitia!</p>
-             <button>Home</button>
+        <div className='barcdes'>
+          <p>
+          Barcelona is a city with a wide range of original leisure options that 
+          encourage you to visit time and time again. Overlooking the Mediterranean Sea, 
+          and famous for Gaudí and other Art Nouveau architecture, 
+          Barcelona is one of Europe's trendiest cities.
+          </p>
+          <button className='barcbtn'>Back</button>
+        </div>
+
+
     </div>
   )
 }
--- src/Components/Itlpage.jsx
+++ src/Components/Itlpage.jsx
@@ -2,16 +2,37 @@ import React from 'react'
 
 function Itlpage() {
   return (
-    <div>Itlpage
+    <div className='itapg'>
+          <h2 className='h2ita'>Italy</h2>
 
+        <div className='italyp'>
 
-        <p>Lorem ipsum dolor sit amet 
-            consectetur adipisicing elit. Iusto quas quaerat a cumque 
-            laboriosam numquam placeat laborum quasi! Dignissimos, voluptas. 
-            At ipsam obcaecati temporibus eum dolorum facilis consequuntur voluptatem earum.
+        <div className='itlimg1'>
+        <img className='itl1' src="https://ik.imagekit.io/8sp02fxgg/italy1.jpg?updatedAt=1686860389793" alt="" />
+        </div>
 
-        </p>
-    </div>
+        <div className='itlimg2'>
+          <img className='itl2' src="https://ik.imagekit.io/8sp02fxgg/italy2.jpg?updatedAt=1686860389741" alt="" />
+        </div>
+
+        <div className='itlimg3'>
+          <img className='itl3' src="https://ik.imagekit.io/8sp02fxgg/italy5.jpg?updatedAt=1686860390703" alt="" />
+        </div>
+
+        </div>
+    
+          <p className='italp'>
+          Italy is a boot-shaped peninsula that juts out of southern Europe into the Adriatic Sea,
+           Tyrrhenian Sea, Mediterranean Sea, and other waters. Its location has played an
+            important <br /> role in its history. 
+          The sea surrounds Italy, and mountains crisscross the interior, dividing it into regions.
+          </p>
+          <br />
+          <button className='italbak'>Back</button>
+
+        </div>
+
+    
   )
 }
 
